7ffe5b3cb776ce343c218ef4006635c753187cf7
Anujv93 <anujv1212@gmail.com>
2026-04-12 20:47:24 +0530

    Sitemap.xml

--- app/(main)/sitemap.ts
+++ app/(main)/sitemap.ts
@@ -112,6 +112,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: 'monthly',
             priority: 0.7,
         },
+        {
+            url: `${baseUrl}/cyber-empowerment`,
+            lastModified: currentDate,
+            changeFrequency: 'monthly',
+            priority: 0.3,
+        },
     ]
 
     // Offers pages
